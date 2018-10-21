9b2974ad95ad54ca139779f5f12ebda9c1ff6238
princesampson <41787597+princesampson@users.noreply.github.com>
2018-10-21 20:30:38 +0200

    Pushed game

---
+++ Nick/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed = 5f; 
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        transform.Translate(movement * Time.deltaTime * moveSpeed);
+	}
+}
---
+++ Nick/Assets/Scripts/SpaceDebris.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceDebris : MonoBehaviour {
+
+    public GameObject [] gameObjects ; 
+    public Transform spawner;
+    public Vector3 spawnValue; 
+
+    int randomInt;
+
+	// Use this for initialization
+	void Start () {
+
+        InvokeRepeating("Inst", 1f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void Inst()
+    {
+        randomInt = Random.Range(0, gameObjects.Length);
+        Vector3 spawnPostition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), 
+            Random.Range(-spawnValue.y, spawnValue.y), 
+            Random.Range(-spawnValue.z, spawnValue.z));
+        GameObject asteroid = Instantiate(gameObjects[randomInt], spawnPostition + spawner.position, spawner.rotation);
+        Rigidbody asteroidRB = asteroid.GetComponent<Rigidbody>();
+        asteroidRB.velocity = transform.TransformDirection(Vector3.forward * 10);
+        asteroid.transform.parent = transform;
+    }
+}
