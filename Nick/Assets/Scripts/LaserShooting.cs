cc1803959fff39f04321c26a1bf1e3447d2c7332
princesampson <41787597+princesampson@users.noreply.github.com>
2018-10-21 21:38:46 +0200

    Added killzone and asteroid damage scripts

---
+++ Nick/Assets/Scripts/LaserShooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserShooting : MonoBehaviour {
+
+    private RaycastHit hit;
+    private Ray ray;
+    public float rayDistance = 4f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
