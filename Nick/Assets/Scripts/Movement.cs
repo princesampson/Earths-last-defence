46ab27241a2bcd1a2aa21711412b0f2ecc895d6e
princesampson <41787597+princesampson@users.noreply.github.com>
2018-10-22 19:26:46 +0200

    Updated movement script, added more stars

--- Nick/Assets/Scripts/Movement.cs
+++ Nick/Assets/Scripts/Movement.cs
@@ -2,21 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
     
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        Vector3 movement = new Vector3(horizontal, vertical, 0f);
         transform.Translate(movement * Time.deltaTime * moveSpeed);
-	}
+
+        Vector3 newPosition = transform.position + movement;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -8, 8); //restricts player movement
+
+        transform.position = newPosition;
+
+
+    }
 }
--- Nick/Assets/Scripts/SpaceDebris.cs
+++ Nick/Assets/Scripts/SpaceDebris.cs
@@ -23,13 +23,13 @@ public class SpaceDebris : MonoBehaviour {
 
     void Inst()
     {
-        randomInt = Random.Range(0, gameObjects.Length);
+        randomInt = Random.Range(0, gameObjects.Length); // randomly spawns objects
         Vector3 spawnPostition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), 
             Random.Range(-spawnValue.y, spawnValue.y), 
-            Random.Range(-spawnValue.z, spawnValue.z));
-        GameObject asteroid = Instantiate(gameObjects[randomInt], spawnPostition + spawner.position, spawner.rotation);
+            Random.Range(-spawnValue.z, spawnValue.z)); // area where objects are spawned
+        GameObject asteroid = Instantiate(gameObjects[randomInt], spawnPostition + spawner.position, spawner.rotation); // spawns objects
         Rigidbody asteroidRB = asteroid.GetComponent<Rigidbody>();
         asteroidRB.velocity = transform.TransformDirection(Vector3.forward * 10);
-        asteroid.transform.parent = transform;
+        asteroid.transform.parent = transform; // parents all objects spawned to spawn point in hierarchy
     }
 }
