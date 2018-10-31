4639e64eb1e8c03420e08a0cb8e761cf49e4600f
princesampson <41787597+princesampson@users.noreply.github.com>
2018-10-31 14:30:57 +0200

    New scene added and updated scene switching  script.

--- Nick/Assets/Scripts/StartGame.cs
+++ Nick/Assets/Scripts/StartGame.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class StartGame : MonoBehaviour {
 
-
+	public void NextScene()
+	{
+		SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+	}
 }
