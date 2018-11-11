7eeb96f2ebd1544ccad34d2ad18ed2b521af12bb
princesampson <41787597+princesampson@users.noreply.github.com>
2018-11-11 07:38:13 +0200

    changed  player attack

--- Nick/Assets/Scripts/LaserShooting.cs
+++ Nick/Assets/Scripts/LaserShooting.cs
@@ -18,21 +18,6 @@ public class LaserShooting : MonoBehaviour {
             Rigidbody laserRB = capsule.GetComponent<Rigidbody>();
             laserRB.velocity = transform.TransformDirection(Vector3.forward * 5);
             transform.parent = transform; // parents spawned object to it's spawned point in the hierarchy 
-
-            /* ray = new Ray(transform.position,transform.forward);
-             Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.green);
-
-             if (Physics.Raycast(ray, out hit))
-             {
-                 if (hit.distance <= rayDistance)
-                 {
-                     print("die");
-                     GameObject capsule = Instantiate(laser, spawner.position, spawner.rotation);
-                     Rigidbody laserRB = capsule.GetComponent<Rigidbody>();
-                     laserRB.velocity = transform.TransformDirection(Vector3.forward * 5);
-                     transform.parent = transform; // parents spawned object to it's spawned point in the hierarchy 
-                 }
-             }*/
         }
        
 
